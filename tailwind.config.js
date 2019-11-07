e9c8d239c579c44d4c01acb9d07f15139f8bec5e
Rob Cameron <rob.cameron@fastmail.com>
2019-11-07 10:45:36 -0800

    Adds tailwind gradient plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,17 @@
 /* See https://tailwindcss.com/docs/configuration for more options */
 
 module.exports = {
-  theme: {},
+  theme: {
+    linearGradients: theme => ({
+      colors: theme("colors")
+    }),
+    radialGradients: theme => ({
+      colors: theme("colors")
+    }),
+    conicGradients: theme => ({
+      colors: theme("colors")
+    })
+  },
   variants: {},
-  plugins: []
+  plugins: [require("tailwindcss-gradients")()]
 };
