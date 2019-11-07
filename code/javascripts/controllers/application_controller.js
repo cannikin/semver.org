4bcda2a8752c8477aade813c99eabe9dc5c0595a
Rob Cameron <rob.cameron@fastmail.com>
2019-11-07 10:45:55 -0800

    Create copy code blocks on the fly

--- code/javascripts/controllers/application_controller.js
+++ code/javascripts/controllers/application_controller.js
@@ -3,19 +3,24 @@
 import { Controller } from "stimulus";
 
 export default class extends Controller {
-  static get targets() {
-    return ["code"];
+  connect() {
+    for (let el of document.getElementsByTagName("code")) {
+      if (el.parentElement.nodeName === "PRE") {
+        let container = el.parentElement.parentElement;
+        let link = document.createElement("a");
+
+        link.href = "#";
+        link.classList.add("block", "text-right", "text-sm");
+        link.dataset.action = "click->application#copy";
+        link.textContent = "Copy to Clipboard";
+        container.appendChild(link);
+      }
+    }
   }
 
   copy(event) {
     event.preventDefault();
-    const targetEl = event.target;
-
-    this.codeTargets.forEach(target => {
-      if (target.dataset.id === targetEl.dataset.targetId) {
-        this.copyContent(target);
-      }
-    });
+    this.copyContent(event.target.previousSibling.previousSibling.children[0]);
   }
 
   copyContent(target) {
